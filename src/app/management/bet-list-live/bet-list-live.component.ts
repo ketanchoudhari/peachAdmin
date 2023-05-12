4f5075abdef9a03b0d7d1b3ba5f8a1db86016a8f
nikihil3 <106944073+nikihil3@users.noreply.github.com>
2023-05-12 18:39:33 +0530

    man component all

---
+++ src/app/management/bet-list-live/bet-list-live.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bet-list-live',
+  templateUrl: './bet-list-live.component.html',
+  styleUrls: ['./bet-list-live.component.css']
+})
+export class BetListLiveComponent {
+
+}
---
+++ src/app/management/bet-list/bet-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bet-list',
+  templateUrl: './bet-list.component.html',
+  styleUrls: ['./bet-list.component.css']
+})
+export class BetListComponent {
+
+}
